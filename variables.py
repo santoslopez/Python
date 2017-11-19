0604c12c44f4edf12e028bd107728aab30515bac
Santos López <santos.lopez@galileo.edu>
2017-11-19 13:02:42 -0600

    Ejemplo de declaracion de variables en Python
    -declaracion de variables
    -mostrar tipo de dato de la variable
    -poner varios saltos de linea

---
+++ variables.py
@@ -0,0 +1,23 @@
+entero  = 1
+print "El numero de la variable entero es ",entero #forma valida de imprimir
+flotante = 2.3 #dependiendo del valor ingresado, entero flotante, double, la variable va guardar ese tipo de dato
+print("El numero de la variable flotante es ",flotante)
+
+texto = "Sigue con el trabajo duro y no desistas"
+print "La variable texto tiene el siguiente contenido\n",texto
+
+division = 9/2
+print "La variable division tiene el siguiente contenido\n",division
+
+saltosLinea = """Este mensaje
+contiene 3
+saltos de
+linea"""
+
+
+print "La variable saltosLinea tiene el siguiente contenido\n",saltosLinea
+
+#para devolver que tipo de dato es la variable
+print "La variable con nombre entero es de tipo ",type(entero)
+print "La variable con nombre flotante es de tipo ",type(flotante)
+print "La variable con nombre texto es de tipo ",type(texto)
